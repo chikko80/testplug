b3d8a7165629c6cd2092510e9c84dbdf5511b316
chikko80 <chikko@Chikkos-MacBook-Pro.fritz.box>
2023-04-06 16:25:19 +0200

    fix events

--- lua/better-window/init.lua
+++ lua/better-window/init.lua
@@ -1,4 +1,5 @@
 -- better-window.nvim.lua
+
 local utils = require("better-window.utils")
 local EditorGroup = require("better-window.manager")
 
@@ -6,18 +7,18 @@ local editor_group = EditorGroup.new()
 
 local function debug()
 	for _, win_id in pairs(editor_group.ordered_stacks) do
+		print("Window ID:", win_id)
 		print("Stack contents:", vim.inspect(editor_group.stacks[win_id]))
 	end
 end
 
 local function setup()
-	-- TODO: rethink about events
 	vim.cmd([[
       augroup LayoutTracker
         autocmd!
+        autocmd WinNew,WinClosed * lua require('better-window').update_layout("WNC")
         autocmd VimEnter * lua require('better-window').update_layout()
         autocmd BufWinEnter * lua require('better-window').update_layout()
-        autocmd WinClosed * lua require('better-window').update_layout()
       augroup END
     ]])
 	-- Create commands
@@ -52,12 +53,11 @@ local function move(direction)
 
 	-- replace the old buffer by latest stack after pop
 	local latest_buffer = editor_group:getLatestBuffer(old_win_id)
-    if latest_buffer == nil then
-        vim.api.nvim_win_close(old_win_id, true)
-    else
-        vim.api.nvim_win_set_buf(old_win_id, latest_buffer)
-    end
-
+	if latest_buffer == nil then
+		vim.api.nvim_win_close(old_win_id, true)
+	else
+		vim.api.nvim_win_set_buf(old_win_id, latest_buffer)
+	end
 
 	-- move buffer and set cursor position
 	vim.api.nvim_win_set_buf(new_win_id, old_buf)
@@ -81,10 +81,16 @@ local function remove_buffer()
 	end
 end
 
-local function update_layout()
+local function update_layout(event)
 	-- get list of open buffers
 	local open_windows = utils.get_open_windows()
 	if open_windows == nil then
+		return
+	end
+
+	-- NOTE: WinNew and WinClosed are triggered too often in the background (probably by other plugins)
+    -- NOTE: Therefore we ignore the event it the layout didn't change
+	if event == "WNC" and #editor_group.ordered_stacks == #open_windows then
 		return
 	end
 
@@ -108,6 +114,7 @@ local function update_layout()
 		editor_group:removeGroupIfWindowRemoved(open_windows)
 	end
 	editor_group:setGroupOrder(open_windows)
+	debug()
 end
 
 return {
